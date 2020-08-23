69db984e9124523ffbe584364428f104f85d868d
James Fleming <james@electronic-quill.net>
2020-08-23 18:16:15 +0200

    Add a trailing slash to the schemas path
    This is now needed to positively identify it as a directory.

--- scripts/docker/create_executable.sh
+++ scripts/docker/create_executable.sh
@@ -1,3 +1,3 @@
 sbcl --userinit sbclrc-docker \
     --eval "(asdf:load-system :syscat)" \
-    --eval "(sb-ext:save-lisp-and-die \"sebcat\" :executable t :toplevel #'(lambda () (syscat::dockerstart :schemapath \"/schemas\")))"
+    --eval "(sb-ext:save-lisp-and-die \"sebcat\" :executable t :toplevel #'(lambda () (syscat::dockerstart :schemapath \"/schemas/\")))"
