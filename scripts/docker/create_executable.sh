6ed5f8098e76f3b10ae1b845c52406c6535639e4
James Fleming <james@electronic-quill.net>
2020-12-11 07:53:01 +0100

    Remove obsolete build files

--- scripts/docker/create_executable.sh
+++
@@ -1,3 +0,0 @@
-sbcl --userinit sbclrc-docker \
-    --eval "(asdf:load-system :syscat)" \
-    --eval "(sb-ext:save-lisp-and-die \"sebcat\" :executable t :toplevel #'(lambda () (syscat::dockerstart :schemapath \"/schemas/\")))"
